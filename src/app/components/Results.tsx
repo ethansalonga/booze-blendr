223d5b8bf8fa5da48bb45d5b82c1cb32a72c5850
Ethan Salonga <ethansalonga28@gmail.com>
2023-10-04 16:19:14 -0400

    feat: style results

--- src/app/components/Results.tsx
+++ src/app/components/Results.tsx
@@ -10,21 +10,40 @@ export default function Results() {
     drink: { name, ingredients, garnishes, image, procedure },
   } = results
 
-  return (
-    <div className="p-4">
-      <div className="border p-4 rounded-md">
-        <h2 className="text-center text-xl mb-2">{message}</h2>
-        {ingredients.map(({ name, measurement, unit }) => (
-          <p key={name}>
-            {measurement} {unit} {name}
-          </p>
-        ))}
-        {garnishes.map((garnish) => (
-          <p key={garnish}>{garnish}</p>
-        ))}
-        {image && <Image src={image} alt={name} width={200} height={200} />}
-        <p>{procedure}</p>
+  if (message)
+    return (
+      <div className="p-4 animate-fadeIn">
+        <div className="p-4">
+          <h2 className="text-center text-xl mb-4">{message}</h2>
+          <div className="mb-2">
+            <p className="font-medium">Ingredients:</p>
+            <ul>
+              {ingredients.map(({ name, measurement, unit }) => (
+                <li key={name}>
+                  {measurement} {unit} {name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="mb-4">
+            <p className="font-medium">Garnishes:</p>
+            <ul>
+              {garnishes.map((garnish) => (
+                <li key={garnish}>{garnish}</li>
+              ))}
+            </ul>
+          </div>
+          {image && (
+            <Image
+              className="rounded-md mx-auto mb-4"
+              src={image}
+              alt={name}
+              width={200}
+              height={200}
+            />
+          )}
+          <p>{procedure}</p>
+        </div>
       </div>
-    </div>
-  )
+    )
 }
